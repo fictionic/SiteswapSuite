7c1f0fc775d23a286c5a33739ddaed1ab9c8981e
Dylan Forbes <dylandforbes@gmail.com>
2014-11-29 13:18:48 -0800

    make parser split up into three cases depending on type of notation

--- Parser.java
+++ Parser.java
@@ -2,35 +2,47 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Parser {
-	private static String validGeneralSiteswapString = "((\\[((\\d|[a-w]|X|[yz])x?)+\\]|(\\d|[a-w]|X|[yz])x?)|\\((\\[((\\d|[a-w]|X|[yz])x?)+\\]|(\\d|[a-w]|X|[yz])x?),(\\[((\\d|[a-w]|X|[yz])x?)+\\]|(\\d|[a-w]|X|[yz])x?)\\)!?)+";
 	private static String validAsyncSiteswapString = "((\\d|[a-w]|X|[yz])x?)+";
+	private static String validSynchronousSiteswapString = "(\\((\\[((\\d|[a-w]|X|[yz])x?)+\\]|(\\d|[a-w]|X|[yz])x?),(\\[((\\d|[a-w]|X|[yz])x?)+\\]|(\\d|[a-w]|X|[yz])x?)\\))+\\*?";
+	private static String validMixedNotationTwoHandedSiteswapString = "((\\[((\\d|[a-w]|X|[yz])x?)+\\]|(\\d|[a-w]|X|[yz])x?)|\\((\\[((\\d|[a-w]|X|[yz])x?)+\\]|(\\d|[a-w]|X|[yz])x?),(\\[((\\d|[a-w]|X|[yz])x?)+\\]|(\\d|[a-w]|X|[yz])x?)\\)!?)+";
 
 	public static boolean checkSyntax(String s) {
-		if(Pattern.matches(validGeneralSiteswapString, s)) {
+		if(Pattern.matches(validMixedNotationTwoHandedSiteswapString, s)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	public static boolean isAsync(String s) {
+	public static String getNotationType(String s) {
 		if(Pattern.matches(validAsyncSiteswapString, s)) {
-			return true;
+			return "async";
+		} else if(Pattern.matches(validSynchronousSiteswapString, s)) {
+			return "sync";
 		} else {
-			return false;
+			return "mixed";
 		}
 	}
 
 	public static Siteswap parse(String s) {
-		if(isAsync(s)) {
-			return parseAsync(s);
-		} else {
-			return parseGeneral(s);
-		}
+		switch(getNotationType(s)) {
+			case "async":
+				return parseAsync(s);
+			case "sync":
+				return parseSync(s);
+			default:
+				//case "mixed":
+				return parseMixed(s);
+		}	
 	}
 	
 	public static Siteswap parseAsync(String s) {
-		Siteswap out = new Siteswap(2);
+		//double string length if it's odd (so e.g. "3" will become (3,0)!(0,3)!) 
+		if(s.length() % 2 == 1) {
+			s += s;
+		}
+		//create new async siteswap
+		Siteswap out = new Siteswap(2, "async");
 		String curToken;
 		int i = 0; //index in input string
 		int b = 0; //index (beat) in output siteswap
@@ -41,22 +53,22 @@ public class Parser {
 			//update current hand
 			curHand = i % 2;
 			switch(curToken) {
-				//if curToken is "[", we're now in a multiplex throw, so add all subsequent tosses to the same site until "]"
+				//if curToken is "[", we're now in a multiplex throw, so add all subsequent tosses to the same hand until "]"
 				case "[":
 					multi = true;
 					out.addBeat();
 					break;
-				//if curToken is "]", we're no longer in a multiplex throw, so add an empty toss to the non-current hand
+					//if curToken is "]", we're no longer in a multiplex throw, so add an empty toss to the non-current hand
 				case "]":
 					multi = false;
-					out.getBeatAt(out.period() - 1).getSiteAt((curHand + 1) % 2).addToss();
+					out.getBeat(out.period() - 1).getHand((curHand + 1) % 2).addToss();
 					b++;
 					break;
-				//if curToken is "x", flip the destination hand of the most recently added toss
+					//if curToken is "x", flip the destination hand of the most recently added toss
 				case "x":
-					out.getBeatAt(out.period() - 1).getSiteAt((curHand + 1) % 2).getLastToss().flipDestHand();
+					out.getBeat(out.period() - 1).getHand((curHand + 1) % 2).getLastToss().flipDestHand();
 					break;
-				//if curToken is anything else, it has to be a throw height (since it matched the regex for async pattern)
+					//if curToken is anything else, it has to be a throw height (since it matched the regex for async pattern)
 				default:
 					int height = throwHeight(curToken);
 					int destHand = (curHand + height) % 2; //0=left, 1=right
@@ -64,14 +76,14 @@ public class Parser {
 						//create new beat
 						Siteswap.Beat newBeat = out.addBeat();
 						//add toss of correct height and destination to current hand
-						newBeat.getSiteAt(curHand).addToss(height, destHand);
+						newBeat.getHand(curHand).addToss(height, destHand);
 						//add empty toss to other hand
-						newBeat.getSiteAt((curHand + 1) % 2).addToss();
+						newBeat.getHand((curHand + 1) % 2).addToss();
 						//increment beat index
 						b++;
 					} else {
 						//add toss of correct height and destination to current hand
-						out.getLastBeat().getSiteAt(curHand).addToss(height, destHand);
+						out.getLastBeat().getHand(curHand).addToss(height, destHand);
 					}
 					break;
 			}
@@ -81,8 +93,71 @@ public class Parser {
 		return out;
 	}
 
+	public static Siteswap parseSync(String s) {
+		//create new sync siteswap
+		Siteswap out = new Siteswap(2, "sync");
+		int i = 0; //index in index string
+		int b = 0; //index of beat within output siteswap
+		int curHand = 0;
+		String curToken;
 
-	public static int throwHeight(String h) {
+		while(i < s.length()) {
+			curToken = ((Character)s.charAt(i)).toString();
+			switch(curToken) {
+				case "(":
+					//create new beat
+					out.addBeat();
+					curHand = 0;
+					break;
+				case ",":
+					curHand = 1;
+					break;
+				case ")":
+					//increase beat index by 2, since every (a,b) counts as two beats
+					b += 2;
+					break;
+				case "[":
+					//doesn't matter whether we're in a multiplex pattern
+					// b/c curHand is determined by other notation
+					break;
+				case "]":
+					break;
+				case "x":
+					//toggle destination hand of most recently added toss
+					out.getLastBeat().getHand(curHand).getLastToss().flipDestHand();
+					break;
+				case "!":
+					//remove last beat
+					out.removeLastBeat();
+					//decrement beat index
+					b--;
+					break;
+				case "*":
+					//need to make this only star-ify the last non-starified portion of the pattern!!!
+					//(e.g. to prevent the first part of (4,2)*(6,2)* from getting star-ified twice)
+					/*IDEA: keep track of last beat at which addStar was called, call it on a sub-pattern
+					of the siteswap, and annex it to out with Siteswap.annexPattern()*/
+					out.addStar();
+					break;
+				default: //curToken is a throw height
+					int height = throwHeight(curToken);
+					int destHand = (curHand + height) % 2;
+					Siteswap.Beat curBeat = out.getLastBeat();
+					curBeat.getHand(curHand).addToss(height, destHand);
+					break;
+			}
+			i++;
+		}
+
+		return out;
+	}
+
+	private static Siteswap parseMixed(String s) {
+		//later...
+		return null;
+	}
+
+	private static int throwHeight(String h) {
 		if(Pattern.matches("\\d", h)) {
 			return Integer.parseInt(h);
 		} else if(Pattern.matches("([a-w]|[yz])", h)) {
@@ -92,17 +167,136 @@ public class Parser {
 		}
 	}
 
-	public static Siteswap parseGeneral(String s) {
-		return null;
+	private static String reverseThrowHeight(Integer h) {
+		if(h <= 9) {
+			return h.toString();
+		} else if((10 <= h) && (h <= 36) && h != 33) {
+			return (Character.toChars(h - 10 + 97)).toString();
+		} else if(h == 33) {
+			return "X";
+		} else {
+			return "";
+		}
 	}
 
+	//IDEA: make parse() treat async siteswaps as one-handed, and have a method in Siteswap that turns a one-handed ss into a two-handed one
+
+
 	public static String deParse(Siteswap ss) {
-		return null;
+		if(ss.numHands == 2) {
+			switch(ss.type) {
+				case "async":
+					return reduceSiteswapString(deParseAsync(ss));
+				case "sync":
+					return reduceSiteswapString(deParseSync(ss));
+				default:
+					//case "mixed":
+					return reduceSiteswapString(deParseMixed(ss));
+			}
+		} else {
+			//later...
+			return "not yet implemented";
+		}
 	}
-	
+
+	private static String deParseAsync(Siteswap ss) {
+		String out = "";
+		int curHandIndex;
+		Siteswap.Beat curBeat;
+		Siteswap.Beat.Hand curHand;
+		Siteswap.Beat.Hand.Toss curToss;
+		//determine which hand throws first
+		if(ss.getBeat(0).getHand(0).isEmpty()) {
+			curHandIndex = 1;
+		} else {
+			curHandIndex = 0;
+		}
+		//loop through beats of siteswap
+		for(int b=0; b<ss.period(); b++) {
+			curBeat = ss.getBeat(b);
+			curHand = curBeat.getHand(curHandIndex);
+			//see if we need to use multiplex notation
+			if(curHand.numTosses() > 1) {
+				out += "[";
+				//loop through tosses of current hand
+				for(int t=0; t<curHand.numTosses(); t++) {
+					curToss = curHand.getToss(t);
+					out += reverseThrowHeight(curToss.height);
+					//see if the throw goes where it normally does; add a "x" if not
+					if(curToss.destHand != (curToss.startHand + curToss.height) % 2) {
+						out += "x";
+					}
+				}
+				out += "]";
+			} else {
+				curToss = curHand.getLastToss();
+				out += reverseThrowHeight(curToss.height);
+				//see if the throw goes where it normally does; add a "x" if not
+				if(curToss.destHand != (curToss.startHand + curToss.height) % 2) {
+					out += "x";
+				}
+			}
+			//alternate curHandIndex
+			curHandIndex = (curHandIndex + 1) % 2;
+		}
+		return out;
+	}
+
+	private static String deParseSync(Siteswap ss) {
+		String out = "";
+		//loop through beats of siteswap
+		for(int b=0; b<ss.period(); b++) {
+			out += "(";
+			Siteswap.Beat curBeat = ss.getBeat(b);
+			//loop through hands within each beat
+			for(int h=0; h<2; h++) {
+				Siteswap.Beat.Hand curHand = curBeat.getHand(h);
+				//see if we need to add multiplex notation
+				if(curHand.numTosses() > 1) {
+					out += "[";
+					//loop through tosses within hand
+					for(int t=0; t<curHand.numTosses(); t++) {
+						Siteswap.Beat.Hand.Toss curToss = curHand.getToss(t);
+						out += reverseThrowHeight(curToss.height);
+						if(curToss.destHand != (curToss.startHand + curToss.height) % 2) {
+							out += "x";
+						}
+					}
+					out += "]";
+				} else {
+					//account for only toss in hand
+					Siteswap.Beat.Hand.Toss curToss = curHand.getLastToss();
+					out += reverseThrowHeight(curToss.height);
+					if(curToss.destHand != (curToss.startHand + curToss.height) % 2) {
+						out += "x";
+					}
+				}
+				//put a comma if we've just finished doing the left hand
+				if(h == 0) {
+					out += ",";
+				}
+			}
+			out += ")";
+		}
+		return out;
+	}
+
+	private static String deParseMixed(Siteswap ss) {
+		return "";
+	}
+
+	private static String reduceSiteswapString(String s) {
+		//TO DO LATER
+		//(also make it use * notation?)
+		return s;
+	}
+
 	public static void main(String[] args) {
 		if(args.length == 1) {
-			System.out.println(parse(args[0]));
+			Siteswap ss = parse(args[0]);
+			System.out.println(ss);
+			String s = deParse(ss);
+			System.out.println(s);
 		}
 	}
 }
--- Siteswap.java
+++ Siteswap.java
@@ -2,19 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Siteswap {
-	private int numSites;
+	protected int numHands;
+	protected String type;
 
 	private List<Beat> beats;
 
-	public Siteswap(int numSites) {
-		this.numSites = numSites;
+	public Siteswap(int numHands, String type) {
+		this.numHands = numHands;
 		beats = new ArrayList<Beat>();
+		this.type = type;
 	}
 
 	public Beat addBeat() {
 		Beat toReturn = new Beat();
 		beats.add(toReturn);
 		return toReturn;
+	}
+
+	private void addBeat(Beat newBeat) {
+		beats.add(newBeat);
 	}
 
 	public double numBalls() {
@@ -29,7 +35,7 @@ public class Siteswap {
 		return beats.size();
 	}
 
-	public Beat getBeatAt(int index) {
+	public Beat getBeat(int index) {
 		return beats.get(index);
 	}
 
@@ -37,66 +43,145 @@ public class Siteswap {
 		return beats.get(beats.size() - 1);
 	}
 
+	public Siteswap getSubPattern(int startBeat, int endBeat) {
+		//later...
+		return null;
+	}
+
+	public void annexPattern(Siteswap toAnnex) {
+		//later...
+	}
+
+	public void removeLastBeat() {
+		beats.remove(beats.size() - 1);
+	}
+
+	public void addStar() {
+		//this operation only makes sense on two-handed siteswaps
+		if(numHands != 2) {
+			return;
+		}
+		int oldNumBeats = beats.size();
+		//add flipped versions of old beats to end of pattern
+		for(int b=0; b<oldNumBeats; b++) {
+			addBeat(beats.get(b).starBeat());
+		}
+	}
+
+	public Siteswap sprungify() {
+		return null;
+		//later...
+	}
+
 	public String toString() {
 		return beats.toString();
 	}
 
 	protected class Beat {
-		protected List<Site> sites;
+		protected List<Hand> hands;
 		//protected int beatIndex; //not sure if I need this...
 
 		public Beat() {
-			sites = new ArrayList<Site>();
-			for(int i=0; i<numSites; i++) {
-				sites.add(new Site(i));
+			hands = new ArrayList<Hand>();
+			for(int i=0; i<numHands; i++) {
+				hands.add(new Hand(i));
 			}
+		}
+
+		private Beat(List<Hand> handList) {
+			hands = handList;
 		}
 
 		public int totalBeatValue() {
 			int total = 0;
-			for(Site s : sites) {
-				total += s.totalSiteValue();
+			for(Hand s : hands) {
+				total += s.totalHandValue();
 			}
 			return total;
 		}
 
-		public Site getSiteAt(int index) {
-			return sites.get(index);
+		public int numHands() {
+			return hands.size();
+		}
+
+		public Hand getHand(int index) {
+			return hands.get(index);
+		}
+
+		private Beat starBeat() {
+			if(numHands != 2) {
+				//this should never happen, b/c the parent method returns void...
+				return this;
+			}
+			//since there is no addHand() method (because that wouldn't make sense in any situation where you aren't just creating a new beat)
+			Beat newBeat = new Beat();
+			List<Hand> newHandList = new ArrayList<Hand>();
+			//add old right hand as new left hand, and vice-versa
+			newHandList.add(hands.get(1).starHand());
+			newHandList.add(hands.get(0).starHand());
+			return new Beat(newHandList);
 		}
 
 		public String toString() {
-			return sites.toString();
+			return hands.toString();
 		}
 
-		protected class Site {
+		protected class Hand {
 			protected List<Toss> tosses;
-			protected int siteIndex;
+			protected int handIndex;
+			private boolean isEmpty;
 
-			public Site(int siteIndex) {
-				tosses = new ArrayList<Toss>();
-				this.siteIndex = siteIndex;
+			public Hand(int handIndex) {
+				this.tosses = new ArrayList<Toss>();
+				this.handIndex = handIndex;
+				this.isEmpty = true;
 			}
-			
-			public int totalSiteValue() {
+
+			public int totalHandValue() {
 				int total = 0;
 				for(Toss t : tosses) {
-					total += t.height();
+					total += t.height;
 				}
 				return total;
 			}
-			public void addToss(int height, int destSite) {
-				tosses.add(new Toss(siteIndex, height, destSite));	
+			public void addToss(int height, int destHand) {
+				tosses.add(new Toss(handIndex, height, destHand));	
+				if(height != 0) {
+					isEmpty = false;
+				}
 			}
 
 			public void addToss() {
-				tosses.add(new Toss(siteIndex));
+				tosses.add(new Toss(handIndex));
+			}
+
+			private void addToss(Toss newToss) {
+				tosses.add(newToss);
+				if(newToss.height != 0) {
+					isEmpty = false;
+				}
+			}
+
+			private Hand starHand() {
+				//flip hand index
+				int newHandIndex = (handIndex + 1) % 2;
+				Hand newHand = new Hand(newHandIndex);
+				//add a copy of all tosses within this hand with altered startHand and destHand values
+				for(Toss t : tosses) {
+					newHand.addToss(t.starToss(newHandIndex));
+				}
+				return newHand;
 			}
 
 			public int numTosses() {
 				return tosses.size();
 			}
 
-			public Toss getTossAt(int index) {
+			public boolean isEmpty() {
+				return isEmpty;
+			}
+
+			public Toss getToss(int index) {
 				return tosses.get(index);
 			}
 
@@ -109,41 +194,38 @@ public class Siteswap {
 			}
 
 			protected class Toss {
-				protected int startSite;
+				protected int startHand;
 				protected int height;
-				protected int destSite;
+				protected int destHand;
 
-				public Toss(int startSite, int height, int destSite) {
-					this.startSite = startSite;
+				public Toss(int startHand, int height, int destHand) {
+					this.startHand = startHand;
 					this.height = height;
-					this.destSite = destSite;
+					this.destHand = destHand;
 				}
 
-				public Toss(int startSite) {
+				public Toss(int startHand) {
 					this.height = 0;
-					this.startSite = startSite;
-					this.destSite = 0;
+					this.startHand = startHand;
+					this.destHand = 0;
 				}
 
-				public int height() {
-					return height;
-				}
-				public int destSite() {
-					return destSite;
-				}
-
-				public void setDestSite(int newDestSite) {
-					this.destSite = newDestSite;
+				public void setDestHand(int newDestHand) {
+					this.destHand = newDestHand;
 				}
 
 				public void flipDestHand() {
-					this.destSite = (this.destSite + 1) % 2;
+					this.destHand = (this.destHand + 1) % 2;
+				}
+
+				private Toss starToss(int newHandIndex) {
+					return new Toss(newHandIndex, height, (destHand + 1) % 2);
 				}
 
 				public String toString() {
 					List<Integer> tossList = new ArrayList<Integer>();
 					tossList.add(height);
-					tossList.add(destSite);
+					tossList.add(destHand);
 					return tossList.toString();
 				}
 			}
