84f7a3e8abdda888c2334cfe73eb174905ba0c93
seeegma <dylandforbes@gmail.com>
2016-01-11 20:32:48 -0600

    make Transition.compute only accept State objects

--- Transition.java
+++ Transition.java
@@ -4,10 +4,6 @@ abstract class Transition extends MutableSiteswap {
 
 	private int firstCatchIndex = -1;
 	
-	static Transition compute(Siteswap from, Siteswap to, boolean allowExtraSqueezeCatches, boolean generateBallAntiballPairs) {
-		return compute(new State(from), new State(to), allowExtraSqueezeCatches, generateBallAntiballPairs);
-	}
-
 	static Transition compute(State from, State to, boolean allowExtraSqueezeCatches, boolean generateBallAntiballPairs) {
 		// determine which subclass constructor to call
 		if(allowExtraSqueezeCatches) {
