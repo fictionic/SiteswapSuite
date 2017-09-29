1b656d041a1301b661577bbb75573f8628cf10d0
Dylan Forbes <dylandforbes@gmail.com>
2017-09-28 17:16:10 -0700

    add analyze() and defaultNotationType(), and fix the complexnotation regex

--- StateNotation.java
+++ StateNotation.java
@@ -1,5 +1,7 @@
 package siteswapsuite;
 
+import java.util.regex.Pattern;
+
 class InvalidStateNotationException extends InvalidNotationException {
 	String message;
 	InvalidStateNotationException(String notation) {
@@ -10,11 +12,34 @@ class InvalidStateNotationException extends InvalidNotationException {
 	}
 }
 
-public class StateNotation {
+public enum StateNotation {
+
+	SIMPLE,
+	COMPLEX;
 
 	public static String charge = "(-?[0-9a-z])";
 	public static String simpleStateNotation = charge + "+";
-	public static String beat = "(\\(" + charge + "(" +  "," + charge + ")" + "\\))";
-	public static String complexStateNotation = beat + "+";
+	public static String beat = "(\\(" + charge + "(" +  "," + charge + ")*" + "\\))";
+	public static String complexStateNotation = "\\(" + beat + "+\\)";
+
+	public static StateNotation analyze(String string) throws InvalidStateNotationException {
+		if(Pattern.matches(simpleStateNotation, string)) {
+			return StateNotation.SIMPLE;
+		} else if(Pattern.matches(complexStateNotation, string)) {
+			return StateNotation.COMPLEX;
+		} else {
+			throw new InvalidStateNotationException(string);
+		}
+	}
+
+	public static StateNotation defaultNotationType(int numHands) {
+		if(numHands == 0) {
+			return StateNotation.SIMPLE;
+		} else if(numHands > 0) {
+			return StateNotation.COMPLEX;
+		} else {
+			return null;
+		}
+	}
 
 }
