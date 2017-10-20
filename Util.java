1db173944cce287bfd02030e1a79c965956215d0
Dylan Forbes <dylandforbes@gmail.com>
2017-10-19 17:31:22 -0700

    add flexibility to Util.printf: println vs print

--- Util.java
+++ Util.java
@@ -10,21 +10,35 @@ public class Util {
 		DEBUG
 	}
 	public static DebugLevel debugLevel = DebugLevel.INFO;
+	public static void printf(Object toPrint, boolean newLine) {
+		printf(toPrint, DebugLevel.INFO, newLine);
+	}
 	public static void printf(Object toPrint) {
-		printf(toPrint, DebugLevel.INFO);
+		printf(toPrint, true);
 	}
 	public static void printf(Object toPrint, DebugLevel minLevel) {
+		printf(toPrint, minLevel, true);
+	}
+	public static void printf(Object toPrint, DebugLevel minLevel, boolean newLine) {
 		if(debugLevel.compareTo(minLevel) >= 0) {
 			PrintStream ps; 
-			if(minLevel == DebugLevel.DEBUG) {
+			if(minLevel == DebugLevel.DEBUG || minLevel == DebugLevel.ERROR) {
 				ps = System.err;
 			} else {
 				ps = System.out;
 			}
 			if(toPrint == null) {
-				ps.println("{null}");
+				if(newLine) {
+					ps.println("{null}");
+				} else {
+					ps.print("{null}");
+				}
 			} else {
-				ps.println(toPrint);
+				if(newLine) {
+					ps.println(toPrint);
+				} else {
+					ps.print(toPrint);
+				}
 			}
 		}
 	}
