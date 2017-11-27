b346ac07b4e99e57b2a19652af00554cd42b5077
Dylan Forbes <dylandforbes@gmail.com>
2017-11-27 10:42:46 -0800

    fix codepath problem by adding Chain.prepare(); add ChainOutput but don't integrate yet

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,12 +21,14 @@ class ArgumentCollection {
 	List<String> strings;
 	String followUpString;
 	int followUpInt;
+
 	private ArgumentCollection() {
 		this.head = Argument.INVALID_TOKEN;
 		this.options = new ArrayList<>();
 		this.ints = new ArrayList<>();
 		this.strings = new ArrayList<>();
 	}
+
 	static ArgumentCollection parse(String str) {
 		ArgumentCollection ret = new ArgumentCollection();
 		boolean isLongOption;
@@ -87,6 +89,7 @@ class ArgumentCollection {
 		}
 		return ret;
 	}
+
 	// for debugging
 	public String toString() {
 		String ret = "";
@@ -119,10 +122,11 @@ class Command {
 		this.chains = new ArrayList<>();
 	}
 
-	void parseArgs(String[] args) throws ParseError, InvalidNotationException, IncompatibleNumberOfHandsException, ImpossibleTransitionException {
+	void parseArgs(String[] args) throws ParseError, InvalidNotationException, IncompatibleNotationException, IncompatibleNumberOfHandsException, ImpossibleTransitionException {
 		String str;
 		for(int i=0; i<args.length; i++) {
 			str = args[i];
+			Util.printf(str, Util.DebugLevel.DEBUG);
 			ArgumentCollection parseResult = ArgumentCollection.parse(str);
 			Argument.Requires headType = parseResult.head.requires;
 			// collect follow-up if necessary
@@ -153,12 +157,14 @@ class Command {
 					if(this.chains.size() < 2) {
 						throw new ParseError("need at least two inputs to compute a transition");
 					}
-					chainInput = new ChainInput(parseResult);
-					this.chains.add(new Chain(chainInput));
+					this.chains.get(this.chains.size()-1).prepare();
+					this.chains.add(this.createChain(parseResult));
 					break;
 				case INPUT:
-					chainInput = new ChainInput(parseResult);
-					this.chains.add(new Chain(chainInput));
+					if(this.chains.size() > 0) {
+						this.chains.get(this.chains.size()-1).prepare();
+					}
+					this.chains.add(this.createChain(parseResult));
 					break;
 				case INVALID_TOKEN:
 					throw new ParseError("invalid token: '" + str + "'");
@@ -173,10 +179,40 @@ class Command {
 		}
 	}
 
-	class ChainInput {
+	Chain createChain(ArgumentCollection parseResult) throws ParseError, InvalidNotationException, ImpossibleTransitionException {
+		ChainInput input = null;
+		// check what type of input we have
+		switch(parseResult.head) {
+			case INPUT:
+				input = new LiteralInput(parseResult);
+				break;
+			case TO_SITESWAP:
+				input = new TransitionInput(parseResult);
+				break;
+			case TRANSITION:
+				input = new TransitionInput(parseResult);
+				break;
+			default:
+				// error
+				Util.printf("IMPOSSIBLE ERROR IN createChain()", Util.DebugLevel.ERROR);
+				System.exit(1);
+		}
+		return new Chain(input);
+	}
+
+	abstract class ChainInput {
 		int index;
-		boolean isTransitionInput;
-		// if single input
+		ChainInput() throws ParseError, InvalidNotationException {
+			this.index = chains.size();
+		}
+		abstract void addArg(ArgumentCollection arg) throws ParseError;
+		abstract String print();
+		abstract void process() throws InvalidNotationException, IncompatibleNotationException, IncompatibleNumberOfHandsException, ImpossibleTransitionException;
+		abstract Link getLink();
+		public abstract String toString();
+	}
+
+	class LiteralInput extends ChainInput {
 		String inputNotation;
 		String prefix = null;
 		boolean isState = false;
@@ -185,77 +221,136 @@ class Command {
 		int numHands = -1;
 		int startHand = -1;
 		boolean keepZeroes = false;
-		// if double input
-		Chain from, to;
+
+		LiteralInput(ArgumentCollection parsedArgs) throws ParseError, InvalidNotationException {
+			super();
+			this.inputNotation = parsedArgs.followUpString;
+			// check for notation type indicators
+			if(this.inputNotation.length() > 2 && this.inputNotation.charAt(2) == ':') {
+				this.prefix = this.inputNotation.substring(0,2);
+				this.inputNotation = this.inputNotation.substring(3,this.inputNotation.length());
+				if(this.prefix.equals("ss")) {
+					this.isState = false;
+				} else if(this.prefix.equals("st")) {
+					this.isState = true;
+				} else {
+					throw new InvalidNotationException("unrecognized input prefix: " + this.prefix);
+				}
+			}
+			// get input options
+			int intIndex = 0;
+			for(int i=0; i<parsedArgs.options.size(); i++) {
+				Argument opt = parsedArgs.options.get(i);
+				switch(opt) {
+					case NUM_HANDS:
+						this.numHands = parsedArgs.ints.get(intIndex++);
+						i++;
+						break;
+					case START_HAND:
+						this.startHand = parsedArgs.ints.get(intIndex++);
+						i++;
+						break;
+					case KEEP_ZEROES:
+						this.keepZeroes = true;
+						break;
+					default:
+						throw new ParseError("option '" + opt.longForm + "' is not an input option");
+				}
+			}
+		}
+
+		void addArg(ArgumentCollection arg) throws ParseError {
+			switch(arg.head) {
+				case NUM_HANDS:
+					this.numHands = arg.followUpInt;
+					break;
+				case START_HAND:
+					this.startHand = arg.followUpInt;
+					break;
+				case KEEP_ZEROES:
+					this.keepZeroes = true;
+					break;
+				default:
+					throw new ParseError("option '" + arg.head.longForm + "' is not an input option");
+			}
+		}
+
+		void process() throws InvalidNotationException, IncompatibleNotationException, IncompatibleNumberOfHandsException {
+			Util.printf("processing " + this, Util.DebugLevel.DEBUG);
+			// if state or siteswap was explicitly indicated
+			if(this.prefix != null) {
+				if(this.isState) {
+					this.notatedState = NotatedState.parse(this.inputNotation, this.numHands, this.startHand);
+				} else {
+					this.notatedSiteswap = NotatedSiteswap.parse(this.inputNotation, this.numHands, this.startHand, this.keepZeroes);
+				}
+			} else {
+				try {
+					this.notatedSiteswap = NotatedSiteswap.parse(this.inputNotation, this.numHands, this.startHand, this.keepZeroes);
+					this.isState = false;
+				} catch(InvalidSiteswapNotationException e) {
+					try {
+						this.notatedState = NotatedState.parse(this.inputNotation, this.numHands, this.startHand);
+						this.isState = true;
+					} catch(InvalidStateNotationException e2) {
+						throw new InvalidNotationException("could not interpret input '" + this.inputNotation + "' as valid siteswap or state notation");
+					}
+				}
+			}
+		}
+
+		Link getLink() {
+			Link link = new Link();
+			link.isState = this.isState;
+			if(link.isState) {
+				link.state = this.notatedState.state;
+			} else {
+				link.siteswap = this.notatedSiteswap.siteswap;
+			}
+			return link;
+		}
+
+		public String print() {
+			StringBuilder ret = new StringBuilder("INPUT "); ret.append(this.index); ret.append(":\n");
+			ret.append(" type: literal\n");
+			ret.append(" notation: '"); ret.append(this.inputNotation); ret.append("'\n");
+			if(this.numHands != -1) {
+				ret.append(" numHands: ");
+				ret.append(this.numHands);
+				ret.append("\n");
+			}
+			if(this.startHand != -1) {
+				ret.append(" startHand: ");
+				ret.append(this.startHand);
+				ret.append("\n");
+			}
+			return ret.toString();
+		}
+
+		public String toString() {
+			return "literal input chain #" + this.index;
+		}
+	}
+
+	class TransitionInput extends ChainInput {
+		int fromIndex = -1, toIndex = -1;
 		int maxTransitions = -1;
 		int minTransitionLength = -1;
 		boolean allowExtraSqueezeCatches = false;
 		boolean generateBallAntiballPairs = false;
 		boolean unAntitossifyTransitions = false;
 		int selectTransition = 0;
-		// constructors
-		ChainInput(ArgumentCollection parsedArgs) throws ParseError, InvalidNotationException {
-			this.index = chains.size();
-			// check what type of input we have
-			switch(parsedArgs.head) {
-				case INPUT:
-					this.inputNotation = parsedArgs.followUpString;
-					// check for notation type indicators
-					if(this.inputNotation.length() > 2 && this.inputNotation.charAt(2) == ':') {
-						this.prefix = this.inputNotation.substring(0,2);
-						this.inputNotation = this.inputNotation.substring(3,this.inputNotation.length());
-						if(this.prefix.equals("ss")) {
-							this.isState = false;
-						} else if(this.prefix.equals("st")) {
-							this.isState = true;
-						} else {
-							throw new InvalidNotationException("unrecognized input prefix: " + this.prefix);
-						}
-					}
-					// get input options
-					int intIndex = 0;
-					for(int i=0; i<parsedArgs.options.size(); i++) {
-						Argument opt = parsedArgs.options.get(i);
-						switch(opt) {
-							case NUM_HANDS:
-								this.numHands = parsedArgs.ints.get(intIndex++);
-								i++;
-								break;
-							case START_HAND:
-								this.startHand = parsedArgs.ints.get(intIndex++);
-								i++;
-								break;
-							case KEEP_ZEROES:
-								this.keepZeroes = true;
-								break;
-							default:
-								throw new ParseError("option '" + opt.longForm + "' is not an input option");
-						}
-					}
-					break;
-				case TO_SITESWAP:
-					this.to = getChain(-1);
-					this.from = getChain(-1);
-					this.isTransitionInput = false;
-					break;
-				case TRANSITION:
-					this.to = getChain(-1);
-					this.from = getChain(-2);
-					this.isTransitionInput = true;
-					break;
-				default:
-					// error
-					Util.printf("IMPOSSIBLE ERROR IN ChainInput()", Util.DebugLevel.ERROR);
-					System.exit(1);
-			}
-		}
-		void getTransitionOptions(ArgumentCollection parsedArgs) throws ParseError {
+		List<Siteswap> transitions;
+		NotatedSiteswap outputSiteswap;
+
+		TransitionInput(ArgumentCollection parsedArgs) throws ParseError, InvalidNotationException {
+			super();
 			int intIndex = 0;
 			for(int i=0; i<parsedArgs.options.size(); i++) {
 				Argument opt = parsedArgs.options.get(i);
 				switch(opt) {
 					case MAX_TRANSITIONS:
-						this.numHands = parsedArgs.ints.get(intIndex++);
+						this.maxTransitions = parsedArgs.ints.get(intIndex++);
 						i++;
 						break;
 					case ALLOW_EXTRA_SQUEEZE_CATCHES:
@@ -269,79 +364,90 @@ class Command {
 				}
 			}
 		}
-		void process() throws InvalidNotationException, IncompatibleNumberOfHandsException, ImpossibleTransitionException {
-			if(this.isTransitionInput) {
-				// IN PROGRESS
-				// for now just take output from two most recent Chains as inputs
-				Link l1 = getChain(-3).getLastLink(), l2 = getChain(-2).getLastLink();
-				State from = l1.getState();
-				State to = l2.getState();
-				TransitionFinder tf = new TransitionFinder(from, to);
-				TransitionResults tr = tf.findTransitions(this.minTransitionLength, this.maxTransitions, this.allowExtraSqueezeCatches, this.generateBallAntiballPairs);
-				if(this.unAntitossifyTransitions) {
-					// tr.unAntitossify(prefix, suffix);
-				}
-				GeneralizedTransition gt = tr.getGeneralizedTransition();
-				List<Siteswap> transitions = tr.getTransitions();
-				Util.printf("\\---> TRANSITION:", Util.DebugLevel.INFO);
-				for(Siteswap ss : transitions) {
-					Util.printf(ss, Util.DebugLevel.INFO);
-				}
-				// save one as output
-				this.notatedSiteswap = NotatedSiteswap.assembleAutomatic(transitions.get(this.selectTransition));
-			} else {
-				// if state or siteswap was explicitly indicated
-				if(this.prefix != null) {
-					if(this.isState) {
-						this.notatedState = NotatedState.parse(this.inputNotation, this.numHands, this.startHand);
-					} else {
-						this.notatedSiteswap = NotatedSiteswap.parse(this.inputNotation, this.numHands, this.startHand, this.keepZeroes);
-					}
-				} else {
-					try {
-						this.notatedSiteswap = NotatedSiteswap.parse(this.inputNotation, this.numHands, this.startHand, this.keepZeroes);
-						this.isState = false;
-					} catch(InvalidSiteswapNotationException e) {
-						try {
-							this.notatedState = NotatedState.parse(this.inputNotation, this.numHands, this.startHand);
-							this.isState = true;
-						} catch(InvalidStateNotationException e2) {
-							throw new InvalidNotationException("could not interpret input '" + this.inputNotation + "' as valid siteswap or state notation");
-						}
-					}
-				}
+
+		void addArg(ArgumentCollection parsedArgs) throws ParseError {
+			switch(parsedArgs.head) {
+				case MAX_TRANSITIONS:
+					this.maxTransitions = parsedArgs.followUpInt;
+					break;
+				case ALLOW_EXTRA_SQUEEZE_CATCHES:
+					this.allowExtraSqueezeCatches = true;
+					break;
+				case GENERATE_BALL_ANTIBALL_PAIRS:
+					this.generateBallAntiballPairs = true;
+					break;
+				default:
+					throw new ParseError("option '" + parsedArgs.head.longForm + "' is not a transition option");
 			}
 		}
+
+		void process() throws ImpossibleTransitionException {
+			Util.printf("processing " + this, Util.DebugLevel.DEBUG);
+			// by default just take output from two most recent Chains as inputs
+			if(this.fromIndex == -1) {
+				this.fromIndex = this.index - 2;
+			}
+			if(this.toIndex == -1) {
+				this.toIndex = this.index - 1;
+			}
+			Link l1 = getChain(this.fromIndex).getLastLink(), l2 = getChain(this.toIndex).getLastLink();
+			State from = l1.getState();
+			State to = l2.getState();
+			TransitionFinder tf = new TransitionFinder(from, to);
+			TransitionResults tr = tf.findTransitions(this.minTransitionLength, this.maxTransitions, this.allowExtraSqueezeCatches, this.generateBallAntiballPairs);
+			if(this.unAntitossifyTransitions) {
+				// tr.unAntitossify(prefix, suffix);
+			}
+			GeneralizedTransition gt = tr.getGeneralizedTransition();
+			this.transitions = tr.getTransitions();
+			// save one as output
+			this.outputSiteswap = NotatedSiteswap.assembleAutomatic(transitions.get(this.selectTransition));
+		}
+
 		Link getLink() {
-			Link link = new Link();
-			link.isState = this.isState;
-			if(link.isState) {
-				link.state = this.notatedState.state;
-			} else {
-				link.siteswap = this.notatedSiteswap.siteswap;
-			}
-			return link;
+			Link ret = new Link();
+			ret.isState = false;
+			ret.siteswap = this.outputSiteswap.siteswap;
+			return ret;
 		}
+
 		public String print() {
 			StringBuilder ret = new StringBuilder("INPUT "); ret.append(this.index); ret.append(":\n");
-			if(this.isTransitionInput) {
-				ret.append(" type: transition\n");
-				ret.append(" [TODO]\n");
-			} else {
-				ret.append(" type: literal\n");
-				ret.append(" notation: '"); ret.append(this.inputNotation); ret.append("'\n");
-				if(this.numHands != -1) {
-					ret.append(" numHands: ");
-					ret.append(this.numHands);
-					ret.append("\n");
-				}
-				if(this.startHand != -1) {
-					ret.append(" startHand: ");
-					ret.append(this.startHand);
-					ret.append("\n");
-				}
+			ret.append(" type: transition\n");
+			ret.append(" from: output "); ret.append(this.fromIndex); ret.append("\n");
+			ret.append(" to: output "); ret.append(this.toIndex); ret.append("\n");
+			ret.append(" results:\n");
+			for(Siteswap ss : this.transitions) {
+				ret.append("  " + ss.toString() + "\n");
 			}
 			return ret.toString();
+		}
+
+		public String toString() {
+			return "transition input chain #" + this.index;
+		}
+	}
+
+	class ChainOutput {
+		boolean isState;
+		NotatedSiteswap notatedSiteswap;
+		NotatedState notatedState;
+
+		ChainOutput(Link link) {
+			this.isState = link.isState;
+			if(this.isState) {
+				this.notatedState = NotatedState.assembleAutomatic(link.state);
+			} else {
+				this.notatedSiteswap = NotatedSiteswap.assembleAutomatic(link.siteswap);
+			}
+		}
+
+		String print() {
+			if(this.isState) {
+				return this.notatedState.print();
+			} else {
+				return this.notatedSiteswap.print();
+			}
 		}
 	}
 
@@ -413,33 +519,49 @@ class Command {
 			}
 			return ret.toString();
 		}
+
+		String print() {
+			if(this.isState) {
+				return this.state.toString();
+			} else {
+				return this.siteswap.toString();
+			}
+		}
 	}
 
 	class Chain {
 		ChainInput input;
 		List<Link> links;
 		boolean acceptingInputOptions;
+
 		Chain() {
 			this.links = new ArrayList<>();
 			this.acceptingInputOptions = true;
 		}
+
 		Chain(ChainInput input) throws InvalidNotationException {
 			this();
 			this.input = input;
 		}
+
 		Link getLastLink() {
 			return this.links.get(this.links.size()-1);
 		}
-		void addArg(ArgumentCollection arg) throws ParseError, InvalidNotationException, IncompatibleNumberOfHandsException, ImpossibleTransitionException {
+		
+		// process input and make first link
+		void prepare() throws InvalidNotationException, IncompatibleNumberOfHandsException, IncompatibleNotationException, ImpossibleTransitionException {
+			this.acceptingInputOptions = false;
+			this.input.process();
+			this.links.add(this.input.getLink());
+		}
+
+		void addArg(ArgumentCollection arg) throws ParseError, InvalidNotationException, IncompatibleNotationException, IncompatibleNumberOfHandsException, ImpossibleTransitionException {
 			if(this.acceptingInputOptions) {
-				if((this.input.isTransitionInput && arg.head.role == Argument.Role.TRANSITION_ROLE)
-						|| (!this.input.isTransitionInput && arg.head.role == Argument.Role.INPUT_ROLE)) {
-					this.addInputArg(arg);
+				if(arg.head.role == Argument.Role.INPUT_ROLE || arg.head.role == Argument.Role.TRANSITION_ROLE) {
+					this.input.addArg(arg);
 				} else {
-					this.acceptingInputOptions = false;
-					// make first link
-					this.input.process();
-					this.links.add(this.input.getLink());
+					// process input and make first link
+					this.prepare();
 					// add argument
 					this.addNonInputArg(arg);
 				}
@@ -451,24 +573,7 @@ class Command {
 				}
 			}
 		}
-		void addInputArg(ArgumentCollection arg) {
-			switch(arg.head) {
-				case NUM_HANDS:
-					this.input.numHands = arg.followUpInt;
-					break;
-				case START_HAND:
-					this.input.startHand = arg.followUpInt;
-					break;
-				case KEEP_ZEROES:
-					this.input.keepZeroes = true;
-					break;
-				case SELECT_TRANSITION:
-					this.input.selectTransition = arg.followUpInt;
-					break;
-				default:
-					break;
-			}
-		}
+
 		void addNonInputArg(ArgumentCollection arg) {
 			switch(arg.head.role) {
 				case INFO_ROLE:
@@ -494,12 +599,11 @@ class Command {
 
 			}
 		}
-		void execute() throws InvalidNotationException, IncompatibleNumberOfHandsException, ImpossibleTransitionException {
-			// process if necessary
+
+		void execute() throws InvalidNotationException, IncompatibleNotationException, IncompatibleNumberOfHandsException, ImpossibleTransitionException {
+			Util.printf("executing " + this, Util.DebugLevel.DEBUG);
 			if(this.acceptingInputOptions) {
-				this.input.process();
-				this.links.add(this.input.getLink());
-				this.acceptingInputOptions = false;
+				this.prepare();
 			}
 			// print input info
 			Util.printf(this.input.print(), Util.DebugLevel.INFO, false);
@@ -556,6 +660,15 @@ class Command {
 			// print info for last link
 			Util.printf(this.getLastLink().printInfo());
 		}
+
+		ChainOutput getOutput() {
+			Link lastLink = this.getLastLink();
+			return new ChainOutput(lastLink);
+		}
+
+		public String toString() {
+			return this.input.toString();
+		}
 	}
 
 	Chain getChain(int index) {
@@ -565,7 +678,7 @@ class Command {
 		return this.chains.get(index);
 	}
 
-	void run() throws InvalidNotationException, IncompatibleNumberOfHandsException, ImpossibleTransitionException {
+	void run() throws InvalidNotationException, IncompatibleNotationException, IncompatibleNumberOfHandsException, ImpossibleTransitionException {
 		for(int i=0; i<this.chains.size(); i++) {
 			this.getChain(i).execute();
 		}
